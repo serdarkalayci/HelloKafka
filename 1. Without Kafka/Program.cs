e0ee1d07d7b65142a1183aebd2581212eb015421
Serdar Kalaycı <serdar.kalayci@d-teknoloji.com.tr>
2019-12-17 22:45:27 +0300

    Initial commit

---
+++ 1. Without Kafka/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+        static DateTime _start;
+        static void Main(string[] args)
+        {
+            _start = DateTime.Now;
+            Console.WriteLine("Production started at: " + _start.ToLongTimeString());
+            for (int i = 0; i < 100; i++) {
+                Produce(i);
+            }
+            var _end = DateTime.Now;
+            Console.WriteLine("Consumption ended at: " + _end.ToLongTimeString());
+            Console.WriteLine((_end - _start).TotalMilliseconds);
+
+        }
+
+        static void Produce(int index) {
+            var message = "Message No " + index;
+            Consume(message);
+        }
+
+        static void Consume(string message) {
+            Thread.Sleep(500);
+            Console.WriteLine(message);
+        }
+    }
+}
